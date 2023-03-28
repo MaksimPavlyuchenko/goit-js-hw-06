34da9b3692e90089959014547d382c7a496059ba
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-03-28 12:53:56 +0300

    done-task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,16 @@
 const input = document.querySelector("#validation-input");
-
+console.log(input.dataset.length);
 input.addEventListener("blur", (event) => {
   const validLength = event.currentTarget.value.trim().length;
-  if (validLength >= input.dataset.length) {
+  if (validLength === 6) {
     if (input.classList.contains("invalid")) {
       input.classList.remove("invalid");
     }
     input.classList.add("valid");
-  } else if (validLength <= input.dataset.length) {
+  } else if (
+    validLength <= input.dataset.length ||
+    validLength >= input.dataset.length
+  ) {
     input.classList.add("invalid");
     if (input.classList.contains("valid")) {
       input.classList.remove("valid");
